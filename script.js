cc9fd87ef12ebb5284020d919dcb8779bd345ea1
Mark <149628810+Undetected-Mark@users.noreply.github.com>
2024-04-22 22:43:24 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Add click event listener to call-to-action button
-    const ctaButton = document.querySelector('.cta-button');
-    ctaButton.addEventListener('click', function () {
-        alert('You clicked the Learn More button!');
-        // You can add additional functionality here
-    });
-});
